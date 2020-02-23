db71d14cf3ecf6c58603d262cfa91e9faa33fcf4
Nate Foster <soundwubz@gmail.com>
2020-02-23 13:31:05 -0500

    add logic to check for other players key selection

--- assets/script/game.js
+++ assets/script/game.js
@@ -110,14 +110,72 @@ function startGame() {
     }, 1000);
 }
 
+function gameOver(winner) {
+    console.log('Winner: ' + winner);
+    if(winner === usrID) {
+        alert("You've Won!");
+    } else {
+        alert("You've lost :(");
+    }
+}
+
 function pushSelection(key) {
     playersRef.update({
         keySelected: key
     });
 
-    // add logic to check for other players key selection
-    // if both have key, compare and return results
-    // else wait for other player
+    allPlayer.once('value', (snap) => { // get players from db
+        let playerObj = snap.val();
+        let keyFlag = 0;
+        let selectedKeys = [];
+        $.each( playerObj, (key, value) => { // check each keySelected value of player
+            console.log(key + ": ");
+            console.log(value);
+            if(value.keySelected != null || value.keySelected != undefined) {
+                keyFlag++;
+                console.log('keyFlag: ' + keyFlag);
+                selectedKeys.push({
+                    id: key,
+                    selection: value.keySelected
+                });
+            }
+        });
+        if(keyFlag === 2) { // if both player made selection
+            console.log('both players selected a key');
+            let winner = compareKeys(selectedKeys);
+            gameOver(winner);
+        } else { // if player is waiting for opposite player to make selection 
+            console.log('a player still needs to make a selection');
+            // make new db ref to list winner of the game
+            // or make it a property on the player
+            // when winner is listed run gameOver function
+        }
+    });
+}
+
+function compareKeys(keys) {
+
+    keyScenarios = [
+        { id: "r", winsAgainst: "s" },
+        { id: "p", winsAgainst: "r" },
+        { id: "s", winsAgainst: "p" }
+    ]
+
+    if(keys[0].selection === keys[1].selection) {
+        console.log('game tie');
+    } else {
+        // game not tied
+        for(i = 0; i < keyScenarios.length; i++) {
+            if(keys[0].selection === keyScenarios[i].id) { // checks for correct key scenario
+                if(keys[1].selection === keyScenarios[i].winsAgainst) { // keys[0] beats keys[1];
+                    return keys[0].id;
+                } else { // keys[1] beats keys[0]
+                    return keys[1].id;
+                }
+            }
+        }
+
+    }
 }
 
 document.onkeyup = function(event) {
